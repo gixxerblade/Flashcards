146968a31c82c551792b3176730ac1ac5e5abe20
gixxerblade <sdclarkie@gmail.com>
2020-08-28 18:09:06 -0400

    Flashcard app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import FlashcardList from "./FlashcardList";
+import './app.css'
+const data = require("./questions.json");
 function App() {
+  const [flashcards, setFlashcards] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashcardList flashcards={flashcards} />
     </div>
   );
 }
---
+++ src/Flashcards.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const Flashcards = ({ flashcard }) => {
+   console.log(flashcard.answer);
+  const [flip, setFlip] = useState(false);
+  return (
+    <div
+      className={`card ${flip ? "flip" : ""}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className="front">
+        {flip ? flashcard.answer : flashcard.question}
+      </div>
+      <ul className="back">
+        {flashcard.answer.map(list=>{
+          return(
+            <li key={flashcard.id}>{list}</li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+};
+export default Flashcards;
